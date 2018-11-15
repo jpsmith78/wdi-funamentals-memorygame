cdbfc9c0b94328fe4cd7541f2a3415f0a7cf7caf
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2018-11-15 17:02:23 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,21 @@ var cards = ["queen", "queen", "king", "king"]
 
 var cardsInPlay = []
 
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2) {
+var checkForMatch = function() {
+	if(cardsInPlay.length === 2) {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("you found a match!");
+		console.log("you found a match!");
 	} else {
-		alert("Sorry, try again.");
-	}
+		console.log("Sorry, try again.");
+	} 
+	};
 };
+
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(1);
